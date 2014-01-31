276706485f6ce03974ce6157735b6781de92ef03
elf <elf198012@gmail.com>
2014-01-31 23:23:55 +0800

    Simply SharedObject

--- include/SharedObject.h
+++ include/SharedObject.h
@@ -6,20 +6,59 @@
 namespace elf{
 
 template<typename T>
-class SharedObject;
+class SharedPointer;
+
+template<typename T>
+class SharedObject{
+public:
+ SharedObject(){
+  fprintf(stderr, "SharedObject(): %lu\n", _nReferences);
+ }
+private:
+ ~SharedObject(){
+  fprintf(stderr, "~SharedObject(): %lu\n", _nReferences);
+ }
+
+ void Reference(){
+  ++_nReferences;
+  fprintf(stderr, "Reference: %lu\n", _nReferences);
+ }
+
+ void Dereference(){
+  fprintf(stderr, "Dereference: %lu\n", _nReferences);
+  if(--_nReferences == 0){
+   get()->~T();
+   delete this;
+  }
+ }
+
+ T *get()const{
+  return (T*)_object;
+ }
+
+ Byte _object[sizeof(T)];
+ size_t _nReferences = 1;
+ friend class SharedPointer<T>;
+};
 
 template<typename T>
 class SharedPointer{
 public:
- SharedPointer(){};
+// SharedPointer(): _pObject(new SharedObject<T>()){
+// }
+
+ SharedPointer(SharedObject<T> *pObject): _pObject(pObject){
+ }
 
  SharedPointer(const SharedPointer &other)
   : _pObject(other._pObject){
-  //fprintf(stderr, "Copy constructor\n");
+  fprintf(stderr, "SharedPointer: Copy constructor\n");
   Reference();
  }
 
- ~SharedPointer(){Dereference();}
+ ~SharedPointer(){
+  Dereference();
+ }
 
  SharedPointer &operator=(const SharedPointer &other){
   //fprintf(stderr, "operator=()\n");
@@ -37,12 +76,16 @@ public:
   return _pObject != other._pObject;
  }
 
+ T *get()const{
+  return (T*)_pObject;
+ }
+
  T &operator*()const{
-  return *(T*)_pObject;
+  return *get();
  }
 
  T *operator->()const{
-  return _pObject;
+  return get();
  }
 
  bool operator==(const SharedPointer &other)const{
@@ -71,10 +114,6 @@ public:
  }
 
 private:
- SharedPointer(SharedObject<T> *pObject): _pObject(pObject){
-//  fprintf(stderr, "SharedPointer(SharedObject<T> *pObject)\n");
- }
-
  void Reference(){
   if(_pObject)
    _pObject->Reference();
@@ -85,32 +124,15 @@ private:
    _pObject->Dereference();
  }
  SharedObject<T> *_pObject = nullptr;
- friend class SharedObject<T>;
 };
 
-template<typename T>
-class SharedObject: public T{
-public:
- static SharedPointer<T> New(const T &object){
-  return SharedPointer<T>(new SharedObject(object));
- }
+#define GET_OVERLOAD_NAME(_1,_2,NAME,...) NAME
+#define NewShared(...) GET_OVERLOAD_NAME(__VA_ARGS__, NewShared2, NewShared1)(__VA_ARGS__)
 
- void Reference(){
-  ++_nReferences;
-  //fprintf(stderr, "Reference: %lu\n", _nReferences);
- }
+#define NewShared1(TYPE)\
+ elf::SharedPointer<TYPE>((elf::SharedObject<TYPE>*)(new (new elf::SharedObject<TYPE>()) TYPE()))
 
- void Dereference(){
-  //fprintf(stderr, "Dereference: %lu\n", _nReferences);
-  if(--_nReferences == 0)
-   delete this;
- }
-
-private:
- SharedObject(const T &object): T(object){}
- ~SharedObject(){}
-
- size_t _nReferences = 1;
-};
+#define NewShared2(TYPE, CONSTRUCTOR_PARAMETERS)\
+ elf::SharedPointer<TYPE>((elf::SharedObject<TYPE>*)(new (new elf::SharedObject<TYPE>()) TYPE(CONSTRUCTOR_PARAMETERS)))
 
 }//namespace elf
